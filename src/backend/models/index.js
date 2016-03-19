b4e809d1479867599fc3c310c780167ccf0a91d5
Brendan Molloy <brendan@bbqsrc.net>
2016-03-19 17:15:43 +1100

    Refactor sequelize connection

--- src/backend/models/index.js
+++ src/backend/models/index.js
@@ -6,7 +6,17 @@ const fs = require("fs"),
       basename = path.basename(module.filename)
 
 const db = {}
-const connection = require("../db/connection.js")
+
+const env = process.env.NODE_ENV || "development"
+const config = require("../../../config/config.json")[env]
+
+let connection
+
+if (config.use_env_variable) {
+  connection = new Sequelize(process.env[config.use_env_variable])
+} else {
+  connection = new Sequelize(config.database, config.username, config.password, config)
+}
 
 fs
   .readdirSync(__dirname)
